83643340e5795f3a07e11419ac96ed7a6e928c74
Frederico Guarana <41083940+FredGuarana@users.noreply.github.com>
2022-04-07 21:17:23 -0300

    Class
    Add Model db_allocation

---
+++ src/main/java/com/project/professorallocation/model/Department.java
@@ -0,0 +1,45 @@
+package com.project.professorallocation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "department")
+public class Department {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column( length = 100, nullable = false )
+	private String name;
+	public Department() {
+		super();
+	}
+
+	public String getNome() {
+		return name;
+	}
+
+	public void setNome(String nome) {
+		this.name = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [nome=" + name + ", id=" + id + "]";
+	}
+
+}
---
+++ src/main/java/com/project/professorallocation/model/Professor.java
@@ -0,0 +1,59 @@
+package com.project.professorallocation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Professor {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100, nullable = false)
+	private String name;
+	
+	@Column(length = 11, nullable = false)
+	private String cpf;
+	private Long department_id;
+	
+	
+	
+	public Professor() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Professor []";
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Long getDepartment_id() {
+		return department_id;
+	}
+	public void setDepartment_id(Long department_id) {
+		this.department_id = department_id;
+	}
+	
+	
+
+}
